472695053e770bd50f120691c8319c4238852914
maggie-5miles <maggie@wespoke.com>
2018-01-10 19:03:46 +0800

    decode raw tx

--- services/handlers/tx.go
+++ services/handlers/tx.go
@@ -2,8 +2,10 @@ package handlers
 
 import (
   "fmt"
+  "bytes"
   "net/http"
   "encoding/hex"
+  "encoding/base64"
 
   "github.com/gorilla/mux"
   "github.com/spf13/viper"
@@ -74,6 +76,14 @@ func searchCoinTxByAccount(w http.ResponseWriter, r *http.Request) {
   searchTx(w, prove, findSender, findReceiver)
 }
 
+func decodeRaw(w http.ResponseWriter, r *http.Request) {
+  buf := new(bytes.Buffer)
+  buf.ReadFrom(r.Body)
+  body := buf.String()
+
+  decodeTx(w, body)
+}
+
 type proof struct {
   Height int64       `json:"height"`
   Data   interface{} `json:"data"`
@@ -84,12 +94,14 @@ func getTx(w http.ResponseWriter, prove bool, bkey []byte) {
   res, err := client.Tx(bkey, prove)
   if err != nil {
     common.WriteError(w, err)
+    return
   }
   var tx sdk.Tx
   err = wire.ReadBinaryBytes(res.Proof.Data, &tx)
   // tx, err := coin.ExtractCoinTx(data)
   if err != nil {
     common.WriteError(w, err)
+    return
   }
   wrap := &proof{res.Height, tx}
   // display
@@ -116,6 +128,25 @@ func searchTx(w http.ResponseWriter, prove bool, queries ...string) {
   }
 }
 
+func decodeTx(w http.ResponseWriter, body string) {
+  data, err := base64.StdEncoding.DecodeString(body)
+  if err != nil {
+    common.WriteError(w, err)
+    return
+  }
+
+  var tx sdk.Tx
+  err = wire.ReadBinaryBytes([]byte(data), &tx)
+  if err != nil {
+    common.WriteError(w, err)
+    return
+  }
+  // display
+  if err := printResult(w, tx); err != nil {
+    common.WriteError(w, err)
+  }
+}
+
 // mux.Router registrars
 
 func RegisterQueryTx(r *mux.Router) error {
@@ -138,6 +169,11 @@ func RegisterSearchCoinTxByAccount(r *mux.Router) error {
   return nil
 }
 
+func RegisterDecodeRaw(r *mux.Router) error {
+  r.HandleFunc("/tx/decode", decodeRaw).Methods("POST")
+  return nil
+}
+
 // RegisterTx is a convenience function to
 // register all the  handlers in this module.
 func RegisterTx(r *mux.Router) error {
@@ -146,6 +182,7 @@ func RegisterTx(r *mux.Router) error {
     RegisterSearchCoinTx,
     registerSearchCoinTxByBlock,
     RegisterSearchCoinTxByAccount,
+    RegisterDecodeRaw,
   }
 
   for _, fn := range funcs {
