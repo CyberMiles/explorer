53f7613928efbc319938a7aff3b04312c6f99259
maggie-5miles <maggie@wespoke.com>
2018-01-12 14:32:44 +0800

    add txfee to coin txs

--- ui/src/components/Account/CoinTxs.js
+++ ui/src/components/Account/CoinTxs.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Segment, Table } from "semantic-ui-react"
+import { Segment, Table, Label } from "semantic-ui-react"
 import { inject, observer } from "mobx-react"
 import { Link } from "react-router-dom"
 
@@ -32,11 +32,13 @@ class CoinTxs extends Component {
             <Table.Row>
               <Table.HeaderCell width={1}>Height</Table.HeaderCell>
               <Table.HeaderCell width={5}>From</Table.HeaderCell>
+              <Table.HeaderCell width={1} />
               <Table.HeaderCell width={5}>To</Table.HeaderCell>
+              <Table.HeaderCell width={1}>TxFee</Table.HeaderCell>
             </Table.Row>
           </Table.Header>
           <Table.Body>
-            {txs.map((tx, index) => <CoinTx key={index} tx={tx.tx} height={tx.height} />)}
+            {txs.map((tx, index) => <CoinTx key={index} height={tx.height} tx={tx.tx} />)}
           </Table.Body>
         </Table>
       </Segment>
@@ -44,22 +46,49 @@ class CoinTxs extends Component {
   }
 }
 
+@inject("coinTxsStore")
 class CoinTx extends Component {
+  getCoinLabel(inputs, outputs, address) {
+    let isFrom = inputs.some(o => o.address.addr === address)
+    let isTo = outputs.some(o => o.address.addr === address)
+    return isFrom && isTo ? "SELF" : isFrom ? "OUT" : isTo ? "IN" : ""
+  }
+  parseTx(tx) {
+    let fee = []
+    if (tx.type === "fee/tx") {
+      fee.push(tx.data.fee)
+      tx = tx.data.tx.data
+    } else {
+      tx = tx.data
+    }
+    return { fee, tx }
+  }
+
   render() {
     const height = this.props.height
-    const { inputs, outputs } = this.props.tx
+    const address = this.props.coinTxsStore.address
+    let { tx, fee } = this.parseTx(this.props.tx)
+    const { inputs, outputs } = tx
+    const label = this.getCoinLabel(inputs, outputs, address)
+
     return (
       <Table.Row verticalAlign="top">
         <Table.Cell>{height}</Table.Cell>
         <Table.Cell>
           {inputs.map((input, index) => (
-            <InputOutput key={index} address={input.sender} coins={input.coins} />
+            <InOut key={index} address={input.address.addr} coins={input.coins} />
           ))}
         </Table.Cell>
         <Table.Cell>
+          <CoinLabel label={label} />
+        </Table.Cell>
+        <Table.Cell>
           {outputs.map((output, index) => (
-            <InputOutput key={index} address={output.receiver} coins={output.coins} />
+            <InOut key={index} address={output.address.addr} coins={output.coins} />
           ))}
+        </Table.Cell>
+        <Table.Cell>
+          <Coins coins={fee} />
         </Table.Cell>
       </Table.Row>
     )
@@ -67,24 +96,19 @@ class CoinTx extends Component {
 }
 
 @inject("coinTxsStore")
-class InputOutput extends Component {
+class InOut extends Component {
   render() {
     const { address, coins } = this.props
-    const DivAddress =
-      this.props.coinTxsStore.address === address ? (
-        <span>{address}</span>
-      ) : (
-        <Link to={"/account/" + address}>{address}</Link>
-      )
+    const me = this.props.coinTxsStore.address === address
 
     return (
       <Table basic="very" compact fixed singleLine>
         <Table.Body>
           <Table.Row verticalAlign="top">
             <Table.Cell width={3} style={{ paddingTop: "0px" }}>
-              {DivAddress}
+              <Address address={address} isLink={!me} />
             </Table.Cell>
-            <Table.Cell width={1} textAlign="right" style={{ paddingTop: "0px" }}>
+            <Table.Cell width={1} style={{ paddingTop: "0px" }}>
               <Coins coins={coins} />
             </Table.Cell>
           </Table.Row>
@@ -92,6 +116,19 @@ class InputOutput extends Component {
       </Table>
     )
   }
+}
+
+const Address = ({ address, isLink }) =>
+  isLink ? <Link to={"/account/" + address}>{address}</Link> : <span>{address}</span>
+
+const CoinLabel = ({ label }) => {
+  const color = label === "IN" ? "green" : label === "OUT" ? "orange" : ""
+  if (color === "") return <div />
+  return (
+    <Label size="small" color={color} horizontal>
+      {label}
+    </Label>
+  )
 }
 
 export default CoinTxs
